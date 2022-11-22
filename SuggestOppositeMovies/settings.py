59c8ed734df0f566daf26818df66a2a2f6d81c4f
asmirp <asmirprepic@gmail.com>
2022-11-23 00:31:19 +0100

    Adjusting STATIC_ROOT

--- SuggestOppositeMovies/settings.py
+++ SuggestOppositeMovies/settings.py
@@ -44,11 +44,12 @@ INSTALLED_APPS = [
     "OppositeSuggestor.apps.OppositesuggestorConfig",
 
     'django_extensions',
-    'whitenoise.runserver_nostatic',
+    
 ]   
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -121,6 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "/static/"
+STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
